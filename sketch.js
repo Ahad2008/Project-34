7a096ae4355c6e366be21dee1e39e53ed7fc5e4c
Ahad2008 <72267510+Ahad2008@users.noreply.github.com>
2021-01-05 14:19:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,17 +23,17 @@ function setup() {
   mConstraint = MouseConstraint.create(engine, options);
   World.add(world, mConstraint);
 
-  pendulum1 = new Pendulum(140, 450);
-  pendulum2 = new Pendulum(200, 450);
-  pendulum3 = new Pendulum(260, 450);
-  pendulum4 = new Pendulum(320, 450);
-  pendulum5 = new Pendulum(380, 450);
+  pendulum1 = new Pendulum(340, 450);
+  pendulum2 = new Pendulum(400, 450);
+  pendulum3 = new Pendulum(460, 450);
+  pendulum4 = new Pendulum(520, 450);
+  pendulum5 = new Pendulum(580, 450);
 
-  sling1 = new Sling(pendulum1.body, { x: 140, y: 200 });
-  sling2 = new Sling(pendulum2.body, { x: 200, y: 200 });
-  sling3 = new Sling(pendulum3.body, { x: 260, y: 200 });
-  sling4 = new Sling(pendulum4.body, { x: 320, y: 200 });
-  sling5 = new Sling(pendulum5.body, { x: 380, y: 200 });
+  sling1 = new Sling(pendulum1.body, { x: 340, y: 200 });
+  sling2 = new Sling(pendulum2.body, { x: 400, y: 200 });
+  sling3 = new Sling(pendulum3.body, { x: 460, y: 200 });
+  sling4 = new Sling(pendulum4.body, { x: 520, y: 200 });
+  sling5 = new Sling(pendulum5.body, { x: 580, y: 200 });
 }
 
 function draw() {
